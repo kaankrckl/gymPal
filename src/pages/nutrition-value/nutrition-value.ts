2109a32bda44f29474d2fe0d826c9ca2f29d0ec3
Kaan Kırcaklı <kaankrckl@gmail.com>
2020-02-13 12:58:59 +0300

    search

--- src/pages/nutrition-value/nutrition-value.ts
+++ src/pages/nutrition-value/nutrition-value.ts
@@ -12,17 +12,25 @@ import { Observable } from 'rxjs/Rx';
 export class NutritionValuesPage {
 
   private link: string = "https://trackapi.nutritionix.com/v2/natural/nutrients";
-  private link2: string= "https://trackapi.nutritionix.com/v2/search/instant";
+  private link2: string= "https://trackapi.nutritionix.com/v2/search/instant?query=";
 
   data: Observable<any>;
   items:any = new Array<any>();
+  result:any = new Array<any>();
+  results:any = new Array<any>();
+  i: any;
+  search: any = "";
+  outsearch:any;
   constructor(public navCtrl: NavController, private http: HttpClient) {
-
+    //this.getToDoList();
+    this.i=0;
+    this.outsearch=true;
   }
 
-  getValue() {
+  getValue(name) {
+    this.outsearch=true;
 // post request for apple nutrition values will be changed
-    this.data = this.http.post(this.link, {query: "apple"}, {headers:{"x-app-id":"7832794a", "x-app-key":"eb702b0c07264c290926d6d8c3c2d998","x-remote-user-id":"0"}});
+    this.data = this.http.post(this.link, {query: name}, {headers:{"x-app-id":"7832794a", "x-app-key":"eb702b0c07264c290926d6d8c3c2d998","x-remote-user-id":"0"}});
     this.data.subscribe(data =>{
       console.log(data);
       this.items=data.foods[0];
@@ -31,10 +39,26 @@ export class NutritionValuesPage {
 
   }
 
-  public getToDoList(){
-    this.http.get('http://localhost/services/service.php').subscribe((response) => {
-       this.items=response;
+  public getResults(search){
+    this.http.get(this.link2+search, {headers:{"x-app-id":"7832794a", "x-app-key":"eb702b0c07264c290926d6d8c3c2d998"}} ).subscribe((response) => {
+       this.result=response;
+       console.log(this.result.common);
+       console.log("liste uzunluğu "+this.result.common.length);
+       console.log("COMMON 0 "+this.result.common[0]);
+      this.results=this.result.common;
    }); 
   }
+
+  onInput(event) {
+    this.outsearch=false;
+    if (this.search.length > 2) {
+      console.log(this.outsearch);
+      
+      this.getResults(this.search);
+    }
+    else
+      this.results = [];
+  }
+
 
 }
